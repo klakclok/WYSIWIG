76df3722df512ac8173174cb7b61e5e8ced9524c
klakclok <96256696+klakclok@users.noreply.github.com>
2023-02-15 22:04:32 +0300

    fix prettier config & naming classes

--- src/views/TextEditor.vue
+++ src/views/TextEditor.vue
@@ -1,156 +1,198 @@
 <template>
-  <div class="text-editor">
-    <div class="wrapper">
-      <div v-if="editor" class="menu">
-        <button class="btn" @click="undoBtn" :disabled="!editor.can().undo()">
-          <img src="/src/assets/svg/icons/redo.svg" alt="icon" />
-        </button>
-        <button class="btn" @click="redoBtn" :disabled="!editor.can().redo()">
-          <img src="/src/assets/svg/icons/undo.svg" alt="icon" />
-        </button>
-        <button class="btn" @click="toggleHeading">
-          <img src="/src/assets/svg/icons/bold.svg" alt="add-head" />
-        </button>
-        <button class="btn" @click="setParagraph">
-          <img src="/src/assets/svg/icons/paragraph.svg" alt="add-paragraph" />
-        </button>
-        <button class="btn" @click="addImage">
-          <img src="/src/assets/svg/icons/img.svg" alt="icon" />
-        </button>
-        <button class="btn-copy__html" @click="copyHtml">
-          Скопировать HTML
-        </button>
-      </div>
-      <editor-content :editor="editor" />
+    <div class="text-editor">
+        <div class="wrapper">
+            <div
+                v-if="editor"
+                class="menu"
+            >
+                <button
+                    :disabled="!editor.can().undo()"
+                    class="menu-button"
+                    @click="undoBtn"
+                >
+                    <img
+                        alt="icon"
+                        src="/src/assets/svg/icons/redo.svg"
+                    />
+                </button>
+                <button
+                    :disabled="!editor.can().redo()"
+                    class="menu-button"
+                    @click="redoBtn"
+                >
+                    <img
+                        alt="icon"
+                        src="/src/assets/svg/icons/undo.svg"
+                    />
+                </button>
+                <button
+                    class="menu-button"
+                    @click="toggleHeading"
+                >
+                    <img
+                        alt="add-head"
+                        src="/src/assets/svg/icons/bold.svg"
+                    />
+                </button>
+                <button
+                    class="menu-button"
+                    @click="setParagraph"
+                >
+                    <img
+                        alt="add-paragraph"
+                        src="/src/assets/svg/icons/paragraph.svg"
+                    />
+                </button>
+                <button
+                    class="menu-button"
+                    @click="addImage"
+                >
+                    <img
+                        alt="icon"
+                        src="/src/assets/svg/icons/img.svg"
+                    />
+                </button>
+                <button
+                    class="menu-button__copy"
+                    @click="copyHtml"
+                >
+                    Скопировать HTML
+                </button>
+            </div>
+            <editor-content :editor="editor" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import StarterKit from "@tiptap/starter-kit";
-import Image from "@tiptap/extension-image";
-import Placeholder from "@tiptap/extension-placeholder";
-import { Editor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from '@tiptap/starter-kit'
+import Image from '@tiptap/extension-image'
+import Placeholder from '@tiptap/extension-placeholder'
+import { Editor, EditorContent } from '@tiptap/vue-3'
 
 export default {
-  components: {
-    EditorContent,
-  },
+    components: {
+        EditorContent,
+    },
 
-  data() {
-    return {
-      editor: null,
-    };
-  },
+    data() {
+        return {
+            editor: null,
+        }
+    },
 
-  methods: {
-    addImage() {
-      const url = window.prompt("URL");
+    mounted() {
+        this.editor = new Editor({
+            extensions: [
+                StarterKit.configure({
+                    history: true,
+                    heading: {
+                        levels: [1],
+                    },
+                    document,
+                    Text,
+                }),
+                Image,
+                Placeholder.configure({
+                    placeholder: 'Пиши сюда...',
+                }),
+            ],
+            autofocus: true,
+            content: ``,
+        })
+    },
 
-      if (url) {
-        this.editor.chain().focus().setImage({ src: url }).run();
-      }
+    beforeUnmount() {
+        this.editor.destroy()
     },
-    copyHtml() {
-      const editorContent = document.querySelector(".ProseMirror");
-      try {
-        navigator.clipboard.writeText(editorContent.innerHTML);
-      } catch (e) {
-        throw e;
-      }
-    },
-    redoBtn() {
-      this.editor.chain().focus().redo().run();
-    },
-    undoBtn() {
-      this.editor.chain().focus().undo().run();
-    },
-    toggleHeading() {
-      this.editor.chain().focus().toggleHeading({ level: 1 }).run();
-    },
-    setParagraph() {
-      this.editor.chain().focus().setParagraph().run();
-    },
-  },
 
-  mounted() {
-    this.editor = new Editor({
-      extensions: [
-        StarterKit.configure({
-          history: true,
-          heading: {
-            levels: [1],
-          },
-          document,
-          Text,
-        }),
-        Image.configure({
-          HTMLAttributes: {
-            class: "image-card",
-          },
-        }),
-        Placeholder.configure({
-          placeholder: "Пиши сюда...",
-        }),
-      ],
-      autofocus: true,
-      content: ``,
-    });
-  },
+    methods: {
+        addImage() {
+            const url = window.prompt('URL')
 
-  beforeUnmount() {
-    this.editor.destroy();
-  },
-};
+            if (url) {
+                this.editor.chain().focus().setImage({ src: url }).run()
+            }
+        },
+        copyHtml() {
+            const editorContent = document.querySelector('.ProseMirror')
+            try {
+                navigator.clipboard.writeText(editorContent.innerHTML)
+            } catch (e) {
+                throw e
+            }
+        },
+        redoBtn() {
+            this.editor.chain().focus().redo().run()
+        },
+        undoBtn() {
+            this.editor.chain().focus().undo().run()
+        },
+        toggleHeading() {
+            this.editor.chain().focus().toggleHeading({ level: 1 }).run()
+        },
+        setParagraph() {
+            this.editor.chain().focus().setParagraph().run()
+        },
+    },
+}
 </script>
 
 <style>
 .text-editor {
-  width: 100%;
-  height: 100vh;
-  background-color: #1e1e1e;
+    width: 100%;
+    height: 100vh;
+    background-color: #1e1e1e;
 }
+
 .wrapper {
-  max-width: 80%;
-  margin: 0 auto;
+    max-width: 80%;
+    margin: 0 auto;
 }
+
 .menu {
-  padding-top: 80px;
-  padding-bottom: 30px;
+    padding-top: 80px;
+    padding-bottom: 30px;
 }
+
 .ProseMirror p {
-  color: #eaeaea;
-  font-family: Roboto;
+    color: #eaeaea;
+    font-family: Roboto;
 }
-.btn {
-  width: 42px;
-  height: 38px;
-  background-color: #282828;
-  border-radius: 5px;
-  margin-right: 12px;
+
+.menu-button {
+    width: 42px;
+    height: 38px;
+    background-color: #282828;
+    border-radius: 5px;
+    margin-right: 12px;
 }
+
 img .ProseMirror-selectednode {
-  outline: 3px solid #68cef8;
+    outline: 3px solid #68cef8;
 }
+
 .ProseMirror p.is-editor-empty:first-child::before {
-  content: attr(data-placeholder);
-  float: left;
-  color: #adb5bd;
-  pointer-events: none;
-  height: 0;
+    content: attr(data-placeholder);
+    float: left;
+    color: #adb5bd;
+    pointer-events: none;
+    height: 0;
 }
+
 h1 {
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 31px;
-  color: #fff;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 31px;
+    color: #fff;
 }
-.btn-copy__html {
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 400;
-  font-size: 15px;
-  color: #639eff;
+
+.menu-button__copy {
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    color: #639eff;
 }
 </style>
